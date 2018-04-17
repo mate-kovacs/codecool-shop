7c9a04b06190d2ffc8f415c22abb99e015b8c84e
Kristóf Egressy <egressyk@gmail.com>
2018-04-17 13:16:16 +0200

    ShoppingCart skeleton

---
+++ src/main/java/com/codecool/shop/model/ShoppingCart.java
@@ -0,0 +1,20 @@
+package com.codecool.shop.model;
+
+import java.util.HashMap;
+
+public class ShoppingCart {
+    private HashMap<Product, Integer> shoppingCartContent;
+
+    public void addItem(int id) {
+
+    }
+
+    public void removeItem(int id) {
+
+    }
+
+    public HashMap getContent() {
+        return shoppingCartContent;
+    }
+
+}
