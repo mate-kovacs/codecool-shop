25cd0b005b32a8259771524716c8766d91025743
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-04-18 10:50:12 +0200

    User can pay the order after the server redirecting to the main page

--- src/main/java/com/codecool/shop/controller/PaymentController.java
+++ src/main/java/com/codecool/shop/controller/PaymentController.java
@@ -1,14 +1,16 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.model.Order;
+import com.codecool.shop.model.User;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/payment"})
@@ -23,12 +25,14 @@ public class PaymentController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User user = req.getSession().getAttribute("UserObject");
+        HttpSession session = req.getSession();
+        User user = (User)session.getAttribute("UserObject");
         for (Order order: user.orders) {
-            if(order.getStatus() == Status.IN_PROGRESS) {
+            if(order.getStatus() == Order.Status.IN_PROGRESS) {
                 order.pay();
+                break;
             }
         }
-        doGet(req, resp);
+        resp.sendRedirect("/");
     }
 }
