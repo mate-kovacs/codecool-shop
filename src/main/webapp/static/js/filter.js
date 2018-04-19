5c0745fc399ba3ff6a2606f26838452f87cc28eb
Tibor Vlasics <tibor.vlasics@gmail.com>
2018-04-19 22:47:40 +0200

    Javascript item template reorganized

--- src/main/webapp/static/js/filter.js
+++ src/main/webapp/static/js/filter.js
@@ -39,28 +39,6 @@ function addFilterListeners() {
     });
 }
 
-// function itemHtmlText(id, title, description, price){
-//
-//         return "<div class=\"item col-xs-4 col-lg-4\">\n" +
-//             "            <div class=\"thumbnail\">\n" +
-//             "                <img class=\"group list-group-image\" src=\"/static/img/product_"+ id +".jpg\" alt=\"\" />\n" +
-//             "                <div class=\"caption\">\n" +
-//             "                    <h4 class=\"group inner list-group-item-heading\">"+title+"</h4>\n" +
-//             "                    <p class=\"group inner list-group-item-text\">"+description+"</p>\n" +
-//             "                    <div class=\"row\">\n" +
-//             "                        <div class=\"col-xs-12 col-md-6\">\n" +
-//             "                            <p class=\"lead\">" + price + "</p>\n" +
-//             "                        </div>\n" +
-//             "                        <div class=\"col-xs-12 col-md-6\">\n" +
-//             "                                <a class=\"btn btn-success add-to-cart\" data-id=\"" + id + "\">\n" +
-//             "                                    Add to cart\n" +
-//             "                                </a>\n" +
-//             "                        </div>\n" +
-//             "                    </div>\n" +
-//             "                </div>\n" +
-//             "            </div>\n" +
-//             "        </div>";
-// }
 
 function changeURL(category, supplier) {
     let urlString = "/?select_category=" + category + "&select_supplier=" + supplier;
@@ -75,13 +53,13 @@ function itemHtmlText(id, title, description, price, supplier){
         "            <h4 class=\"lots-supplier_text\">"+supplier+"</h4>\n" +
         "            <h4 class=\"lots-productname_text\">"+title+"</h4>\n" +
         "            <p class=\"lots-productdesc_text\">"+description+"</p>\n" +
+        "            <div class=\"lots-bot_divider\"></div>\n" +
         "            <div class=\"lots-productcard_footer\">\n" +
-        "                <div class=\"lots-bot_divider\"></div>\n" +
-        "                <div class=\"lots-wrapper\">\n" +
+        "                <div class=\"lots-pricewrapper\">\n" +
         "                    <div class=\"lots_price\">"+price+"</div>\n" +
         "                    <div class=\"lots-currency-icon\"></div>\n" +
-        "                    <div class=\"lots_add-btn add-to-cart\" data-id=\""+id+"\"></div>\n" +
         "                </div>\n" +
         "            </div>\n" +
+        "            <div class=\"lots_add-btn add-to-cart\" data-id=\""+id+"\"></div>\n" +
         "        </div>"
 }
