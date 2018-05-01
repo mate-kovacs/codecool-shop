5105ac95d9b008121dce3312c2ad828112c3caa4
Kristóf Egressy <egressyk@gmail.com>
2018-05-01 20:31:18 +0200

    Added relations and cascading drop

--- src/main/sql/init_db.sql
+++ src/main/sql/init_db.sql
@@ -1,77 +1,77 @@
-DROP TABLE IF EXISTS products;
-CREATE TABLE products
-(
-id SERIAL PRIMARY KEY,
-name varchar(30),
-description text,
-default_price int,
-default_currency varchar(3),
-product_category int,
-supplier int
-);
-
-DROP TABLE IF EXISTS suppliers;
+DROP TABLE IF EXISTS suppliers CASCADE;
 CREATE TABLE suppliers
 (
   id SERIAL PRIMARY KEY,
-  name varchar(36),
-  description text
+  name VARCHAR(36),
+  description TEXT
 );
 
 
-DROP TABLE IF EXISTS product_categories;
+DROP TABLE IF EXISTS product_categories CASCADE;
 CREATE TABLE product_categories
 (
   id SERIAL PRIMARY KEY,
-  name varchar(36),
-  description text,
-  department varchar(36)
+  name VARCHAR(36),
+  description TEXT,
+  department VARCHAR(36)
 );
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS products CASCADE ;
+CREATE TABLE products
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30),
+  description TEXT,
+  default_price INT,
+  default_currency VARCHAR(3),
+  product_category INT REFERENCES product_categories(id),
+  supplier INT REFERENCES suppliers(id)
+);
+
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users
 (
   id SERIAL PRIMARY KEY,
-  username varchar(36),
-  email varchar(36),
-  password text
+  username VARCHAR(36),
+  email VARCHAR(36),
+  password TEXT
 );
 
-DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS orders CASCADE;
 CREATE TABLE orders
 (
   id SERIAL PRIMARY KEY,
-  user_id int references users(id),
-  name varchar(36),
-  email varchar(36),
-  billing_address varchar(150),
-  shipping_address varchar(150),
-  phone varchar(46),
-  creation_date timestamp,
-  status varchar(36),
-  shopping_cart_id int
+  user_id INT REFERENCES users(id),
+  name VARCHAR(36),
+  email VARCHAR(36),
+  billing_address VARCHAR(150),
+  shipping_address VARCHAR(150),
+  phone VARCHAR(46),
+  creation_date TIMESTAMP ,
+  status VARCHAR(36),
+  shopping_cart_id INT
 );
 
-DROP TABLE IF EXISTS shopping_carts;
+DROP TABLE IF EXISTS shopping_carts CASCADE;
 CREATE TABLE shopping_carts
 (
   id SERIAL PRIMARY KEY,
-  creation_date timestamp,
-  status varchar(36)
+  creation_date TIMESTAMP,
+  status VARCHAR(36)
 );
 
-DROP TABLE IF EXISTS user_shopping_cart;
+DROP TABLE IF EXISTS user_shopping_cart CASCADE;
 CREATE TABLE user_shopping_cart
 (
   id SERIAL PRIMARY KEY,
-  user_id int references users(id),
-  shopping_cart_id int references  shopping_carts(id)
+  user_id INT references users(id),
+  shopping_cart_id INT REFERENCES shopping_carts(id)
 );
 
-DROP TABLE IF EXISTS shopping_cart_product;
+DROP TABLE IF EXISTS shopping_cart_product CASCADE;
 CREATE TABLE shopping_cart_product
 (
   id SERIAL PRIMARY KEY,
-  shopping_cart_id int references shopping_carts(id),
-  product_id int references  products(id)
+  shopping_cart_id INT REFERENCES shopping_carts(id),
+  product_id INT REFERENCES  products(id)
 );
