8b2444e0efbb5d0ffb6e9cd390ca7e4ec934a504
Kovács Máté <kismate90@gmail.com>
2018-05-02 15:27:14 +0200

    Add new category with DB implemented

--- src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoDB.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoDB.java
@@ -4,12 +4,19 @@ import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductCategoryDaoDB implements ProductCategoryDao, Queryhandler {
     @Override
     public void add(ProductCategory category) {
-
+        String query = "ISNERT INTO product_categories (name, description, department) VALUES" +
+                " (?, ?, ?);";
+        List<Object> parameters = new ArrayList<>();
+        parameters.add(category.getName());
+        parameters.add(category.getDescription());
+        parameters.add(category.getDepartment());
+        executeDMLQuery(query, parameters);
     }
 
     @Override
