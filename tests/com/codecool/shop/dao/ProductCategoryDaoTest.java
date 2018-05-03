5338c08d5559b9f033128026c86ba6b1a406a08c
Kovács Máté <kismate90@gmail.com>
2018-05-03 14:56:38 +0200

    Tests added for find method of Product Category

--- tests/com/codecool/shop/dao/ProductCategoryDaoTest.java
+++ tests/com/codecool/shop/dao/ProductCategoryDaoTest.java
@@ -1,17 +1,45 @@
 package com.codecool.shop.dao;
 
+import com.codecool.shop.dao.implementation.ProductCategoryDaoDB;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductCategoryDaoTest {
 
-    @Test
-    void add() {
+    private static String testProperties = "test_resources/connection.properties";
+    private static ProductCategoryDao testDao;
+
+    @BeforeEach
+    void setup(){
+        testDao = new ProductCategoryDaoDB(testProperties);
     }
 
     @Test
-    void find() {
+    void test_find_id_0() {
+        assertEquals(null, testDao.find(0));
+    }
+
+    @Test
+    void test_find_id_1() {
+        assertEquals("animal", testDao.find(1).getName());
+    }
+
+    @Test
+    void test_find_id_negative() {
+        assertEquals(null, testDao.find(-7));
+    }
+
+    @Test
+    void test_find_id_too_big() {
+        assertEquals(null, testDao.find(Integer.MAX_VALUE));
+    }
+
+    @Test
+    void add() {
+
     }
 
     @Test
