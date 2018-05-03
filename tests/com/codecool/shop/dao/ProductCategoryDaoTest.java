d0a3d0c87a6438820ca0b31f0c7cda762728de31
Kovács Máté <kismate90@gmail.com>
2018-05-03 16:04:40 +0200

    Tests added for add method

--- tests/com/codecool/shop/dao/ProductCategoryDaoTest.java
+++ tests/com/codecool/shop/dao/ProductCategoryDaoTest.java
@@ -2,8 +2,16 @@ package com.codecool.shop.dao;
 
 import com.codecool.shop.dao.implementation.ProductCategoryDaoDB;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.Queryhandler;
+import com.codecool.shop.model.ProductCategory;
+import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.management.InvalidAttributeValueException;
+
+import java.sql.Connection;
+import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,7 +21,7 @@ class ProductCategoryDaoTest {
     private static ProductCategoryDao testDao;
 
     @BeforeEach
-    void setup(){
+    void setup() {
         testDao = new ProductCategoryDaoDB(testProperties);
     }
 
@@ -38,8 +46,36 @@ class ProductCategoryDaoTest {
     }
 
     @Test
-    void add() {
+    void test_add_null() {
+        ProductCategory category = null;
+        assertThrows(IllegalArgumentException.class, () -> testDao.add(category));
+    }
 
+    @Test
+    void test_add_no_name() {
+        ProductCategory category = new ProductCategory("", "foo", "bar");
+        assertThrows(ValueException.class, () -> testDao.add(category));
+    }
+
+    @Test
+    void test_add_no_department() {
+        ProductCategory category = new ProductCategory("foo", "", "bar");
+        assertThrows(ValueException.class, () -> testDao.add(category));
+    }
+
+    @Test
+    void test_add_no_description() {
+        ProductCategory category = new ProductCategory("foo", "bar", "");
+        assertThrows(ValueException.class, () -> testDao.add(category));
+    }
+
+    @Test
+    void test_add_valid() {
+        ProductCategory category = new ProductCategory("mushroom",
+                "shroom depatrment",
+                "Not an animal, but not a palnt either. Something in between.");
+        testDao.add(category);
+        assertEquals("mushroom", testDao.find(15).getName());
     }
 
     @Test
