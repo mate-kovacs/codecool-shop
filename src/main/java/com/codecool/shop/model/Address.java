f24c4b74c5d8e9e237fb8a4c54f9938f7c488ad3
Kovács Máté <kismate90@gmail.com>
2018-05-09 18:42:36 +0200

    Docstrings added

--- src/main/java/com/codecool/shop/model/Address.java
+++ src/main/java/com/codecool/shop/model/Address.java
@@ -1,11 +1,23 @@
 package com.codecool.shop.model;
 
+/**
+ * Records and stores a custom Address type data.
+ * Address consista of city, country, zip code, and street address.
+ */
 public class Address {
     private String address;
     private String city;
     private String country;
     private int zip;
 
+    /**
+     * Generates an Address type object.
+     *
+     * @param address: street address as a String.
+     * @param city:    city as a String.
+     * @param country: country as a Sring.
+     * @param zip:     zip code as an integer.
+     */
     public Address(String address, String city, String country, int zip) {
         this.address = address;
         this.city = city;
@@ -13,18 +25,38 @@ public class Address {
         this.zip = zip;
     }
 
+    /**
+     * Returns the street address of the Address type data as a String.
+     *
+     * @return: street address as String.
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Returns the city of the Address type data as a String.
+     *
+     * @return: city as String.
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     * Returns the country of the Address type data as a String.
+     *
+     * @return: country as String.
+     */
     public String getCountry() {
         return country;
     }
 
+    /**
+     * Returns the zip code of the Address type data as an integer.
+     *
+     * @return: zip code as integer.
+     */
     public int getZip() {
         return zip;
     }
