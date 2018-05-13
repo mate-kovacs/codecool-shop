b42c36b7fa1d0bce3a1dcdb759746bf4d6a50ed9
Kovács Máté <kismate90@gmail.com>
2018-05-13 20:31:05 +0200

    Docstrings added.

--- src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoDB.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoDB.java
@@ -9,6 +9,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Singleton Data Access Object to access Product Categories, stored in database.
+ */
 public class ProductCategoryDaoDB implements ProductCategoryDao, Queryhandler {
 
     private String connectionConfigPath = "src/main/resources/connection.properties";
@@ -21,22 +24,38 @@ public class ProductCategoryDaoDB implements ProductCategoryDao, Queryhandler {
         return instance;
     }
 
+    /**
+     * Generates the Singleton Data Access Object for Product Categories.
+     * Sets the configuration file to use for building the database connection (to find the correct database).
+     *
+     * @param connectionConfigPath: string filepath starting from the project root, pointing at the config file for the database to connect to.
+     */
     public ProductCategoryDaoDB(String connectionConfigPath) {
         this.connectionConfigPath = connectionConfigPath;
     }
 
+    /**
+     * Generates the Singleton Data Access Object for Product Categories.
+     * Use the DEFAULT configuration file to use for building the database connection (to find the correct database).
+     */
     public ProductCategoryDaoDB() {
     }
 
+    /**
+     * Takes a Product Category Object and inserts it into the database as a new record.
+     * Product Category must have a name, description and department.
+     *
+     * @param category: the Product Category Object to be stored in the database.
+     */
     @Override
     public void add(ProductCategory category) {
         if (category == null) {
             throw new IllegalArgumentException("Null category can not be added.");
-        } else if ("".equals(category.getName())){
+        } else if ("".equals(category.getName())) {
             throw new ValueException("Category must have a name.");
-        } else if ("".equals(category.getDepartment())){
+        } else if ("".equals(category.getDepartment())) {
             throw new ValueException("Category must have a department.");
-        } else if ("".equals(category.getDescription())){
+        } else if ("".equals(category.getDescription())) {
             throw new ValueException("Category must have a description.");
         }
 
@@ -49,6 +68,13 @@ public class ProductCategoryDaoDB implements ProductCategoryDao, Queryhandler {
         executeDMLQuery(query, parameters);
     }
 
+    /**
+     * Searches the database table for Product Categories, and returns the one that has a matching id to the argument.
+     * If no match is found, returns null.
+     *
+     * @param id: integer id to search for in the database.
+     * @return Product Category Object with matching id if found in the database, null otherwise.
+     */
     @Override
     public ProductCategory find(int id) {
         String query = "SELECT * FROM product_categories WHERE id=?;";
@@ -71,6 +97,12 @@ public class ProductCategoryDaoDB implements ProductCategoryDao, Queryhandler {
         return result;
     }
 
+    /**
+     * Removes a Product Category record from the database, if it has a matching id to the argument.
+     * The argument must match one of the valid ids in the database.
+     *
+     * @param id: integer id of the Product Category to search for, and delete from the database.
+     */
     @Override
     public void remove(int id) {
         String query = "DELETE FROM product_categories WHERE id=?;";
@@ -79,7 +111,7 @@ public class ProductCategoryDaoDB implements ProductCategoryDao, Queryhandler {
 
         String tempQuery = "SELECT * FROM product_categories WHERE id=?;";
         List<Map<String, Object>> resultList = executeSelectQuery(tempQuery, parameters);
-        if (resultList.size() == 0){
+        if (resultList.size() == 0) {
             throw new IllegalArgumentException("There is no product category with such id in the database.");
         }
 
@@ -87,12 +119,23 @@ public class ProductCategoryDaoDB implements ProductCategoryDao, Queryhandler {
 
     }
 
+    /**
+     * Removes all Product Categories from the database.
+     */
     @Override
     public void removeAll() {
         String query = "DELETE from product_categories;";
         executeDMLQuery(query);
     }
 
+    /**
+     * Searches the database for Product Categories with matching names to the argument.
+     * The id of the first one's will be returned as an integer.
+     * The name must match at least one Product Category in the database.
+     *
+     * @param name: string name of the Product Category that's id is to be found.
+     * @return Integer id of the Product Category with matching name.
+     */
     @Override
     public Integer findIdByName(String name) {
         String query = "SELECT * FROM product_categories WHERE name=?;";
@@ -103,18 +146,31 @@ public class ProductCategoryDaoDB implements ProductCategoryDao, Queryhandler {
         Integer result = null;
         try {
             result = Integer.parseInt(resultList.get(0).get("id").toString());
-        } catch (IndexOutOfBoundsException ex){
+        } catch (IndexOutOfBoundsException ex) {
             System.out.println(ex.getMessage());
             ex.printStackTrace();
         }
         return result;
     }
 
+    /**
+     * Retruns the default "All" Product Category.
+     *
+     * @return Product Category Object with name "All", and no department of description.
+     */
     @Override
     public ProductCategory getDefaultCategory() {
         return new ProductCategory("All", "", "");
     }
 
+    /**
+     * Takes a list of Product Objects and returns only those, that have a matching Product Category to the argument.
+     * For default Product Category it returns the same List. For any other it returns the modified List.
+     *
+     * @param products: List of Product Objects to be filtered.
+     * @param category: Product Category Object used for filtering the List.
+     * @return List of Product Objects, filtered by Product Category.
+     */
     @Override
     public List<Product> filterProducts(List<Product> products, ProductCategory category) {
         if ((category.toString()).equals(getDefaultCategory().toString())) {
@@ -129,6 +185,12 @@ public class ProductCategoryDaoDB implements ProductCategoryDao, Queryhandler {
         return temp;
     }
 
+    /**
+     * Takes all Product Categories from the database, and returns them as a List of Product Category Objects.
+     * Does not include the Default Product Category.
+     *
+     * @return List of all Product Category Objects stored in the database.
+     */
     @Override
     public List<ProductCategory> getAll() {
         String query = "SELECT * FROM product_categories;";
@@ -149,6 +211,11 @@ public class ProductCategoryDaoDB implements ProductCategoryDao, Queryhandler {
         return results;
     }
 
+    /**
+     * Returns the filepath to the config file, which determines how to build the database connection.
+     *
+     * @return string filepath starting from the project root, pointing at the config file for the database to connect to.
+     */
     @Override
     public String getConnectionConfigPath() {
         return connectionConfigPath;
